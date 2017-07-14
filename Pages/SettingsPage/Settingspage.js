fd1a939f0971f710254a71a8214337329044c618
LG-PC\LG <tjekgus3345@naver.com>
2017-07-15 01:46:09 +0900

    Page폴더에 Settingspage폴더 추가, Settingspage.js 파일 추가 SharePageHoriListView, SharePageHoriListViewItem 파일 추가( 가로 listview)

---
+++ Pages/SettingsPage/Settingspage.js
@@ -0,0 +1,99 @@
+
+import React, { Component, } from 'react';
+
+import {
+    AppRegistry,
+    ListView,
+    Text,
+    StyleSheet,
+    View,
+    Image,
+} from 'react-native';
+
+import SettingsList from 'react-native-settings-list';
+import { Actions } from 'react-native-router-flux';
+
+
+const styles = StyleSheet.create({
+    header : {
+       color: '#000000',
+       backgroundColor:'#f1f1f1',
+       height:30,
+       fontSize:15,
+       alignSelf:'center',
+       textAlignVertical:'center'
+   },
+    item:{
+        fontSize:14,
+        paddingLeft:8,
+    },
+
+});
+
+class Settingspage extends Component {
+    constructor() {
+        super();
+        this.onValueChange = this.onValueChange.bind(this);
+        this.state = {switchValue: false};
+    }
+
+    render() {
+        return (
+            <View style={{backgroundColor: '#f1f1f1', flex: 1}}>
+                <View style={{flex: 1,marginTop:50}}>
+                    <SettingsList borderColor={'#eaeaea'}>
+                        <SettingsList.Header headerText='계정'
+                                             headerStyle={styles.header}/>
+                        <SettingsList.Item
+                            title='별명'
+                            titleInfo='nickname'
+                            itemWidth={40}
+                            titleStyle={styles.item}
+                        />
+                        <SettingsList.Item
+                            title='연결된 계정'
+                            titleInfo='abc1234@gmail.com'
+                            itemWidth={40}
+                            titleStyle={styles.item}
+                        />
+                        <SettingsList.Header headerText='우편함'
+                                             headerStyle={styles.header}/>
+                        <SettingsList.Item title='받을편지'
+                                           hasNavArrow={false}
+                                           underlayColor={'#FFFFFF'}
+                                           itemWidth={40}
+                                           titleStyle={styles.item}
+                        />
+                        <SettingsList.Item title='보낼편지'
+                                           hasNavArrow={false}
+                                           itemWidth={40}
+                                           titleStyle={styles.item}/>
+                        <SettingsList.Item title='예술제안서'
+                                           hasNavArrow={false}
+                                           itemWidth={40}
+                                           titleStyle={styles.item}/>
+
+                        <SettingsList.Header headerText='잡동사니'
+                                             headerStyle={styles.header}/>
+                        <SettingsList.Item title='공지사항'
+                                           itemWidth={40}
+                                           titleStyle={styles.item}/>
+                        <SettingsList.Item title='버전'
+                                           itemWidth={40}
+                                           titleStyle={styles.item}/>
+                        <SettingsList.Item title='약관'
+                                           itemWidth={40}
+                                           titleStyle={styles.item}/>
+                    </SettingsList>
+                </View>
+            </View>
+        );
+    }
+
+    onValueChange(value) {
+        this.setState({switchValue: value});
+    }
+}
+export default Settingspage;
+
+AppRegistry.registerComponent('Settingspage', () => Settingspage);
---
+++ Pages/SharePage/SharePageHoriListViewItem.js
@@ -0,0 +1,98 @@
+/**
+ * Created by LG on 2017-07-15.
+ */
+import React,{
+    Component,
+}from 'react';
+
+import {
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    ListView,
+} from 'react-native';
+
+const styles = StyleSheet.create({
+    shareItem:{
+        flex:1,
+        flexDirection : 'column',
+        backgroundColor : '#FFFFFF',
+        borderBottomColor : '#FFFFFF',
+        borderBottomWidth : 2,
+        padding : 10,
+        paddingTop:180
+    },
+    shareImage:{
+        flex:1,
+        height:500,
+        width:150,
+        resizeMode : 'stretch',
+        paddingTop : 100,
+        paddingBottom:10,
+        alignSelf:'center'
+    },
+    info:{
+        flex : 3,
+        flexDirection : 'column',
+        alignSelf : 'center',
+        paddingTop:10
+    },
+    person:{
+        flex :1,
+        flexDirection: 'row',
+
+    },
+    subject:{
+        fontSize : 18,
+        alignSelf: 'center'
+    },
+    nickname:{
+        fontSize: 13,
+        paddingLeft:10
+
+    },
+    profile:{
+        height:20,
+        width:20,
+        paddingRight:10
+    }
+});
+
+class SharePageHoriListViewItem extends Component{
+    propTypes: {
+        shareImageURL: React.PropTypes.string.isRequired,
+        subject: React.PropTypes.string.isRequired,
+        nickname: React.PropTypes.string.isRequired,
+    };
+
+    render() {
+
+        console.log(this.props.subject);
+        console.log(this.props.shareImageURL);
+        console.log(this.props.nickname);
+        return (
+            <View style={styles.shareItem}>
+                <Image
+                    style={styles.shareImage}
+                    source={{uri: this.props.shareImageURL}}/>
+                <View style={styles.info}>
+                    <Text style={styles.subject}>
+                        {this.props.subject}
+                    </Text>
+                    <View style={styles.person}>
+                        <Image style={styles.profile} source={require('./icons/user.png')}/>
+                        <Text style={styles.nickname}>
+                            {this.props.nickname}
+                        </Text>
+                    </View>
+                </View>
+
+            </View>
+
+
+        );
+    }
+}
+
+export default SharePageHoriListViewItem;
