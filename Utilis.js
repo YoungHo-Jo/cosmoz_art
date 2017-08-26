6a9c50fe450cdd73d540819dd36bbcf2ac75c1ad
YoungHo-Jo <zeroho.jo@gmail.com>
2017-08-26 15:54:04 +0900

    Utilis 수정중

--- Utilis.js
+++ Utilis.js
@@ -28,3 +28,42 @@ export function getWindowSize() {
 }
 
 
+export function getMilliSecs(timeString) {
+  // timeString:: 'mm:ss'
+
+  let dummy = {
+    origin: timeString,
+    min: 0,
+    sec: 0,
+    milli: 0,
+  }
+  if (timeString==='') {
+    console.log('ERROR: timeString is empty')
+    return dummy
+  }
+
+  if (!timeString.includes(':')) {
+    console.log('ERROR: timeString is incorrect')
+    return dummy
+  }
+
+  let arrayOfTimeString = timeString.split(':')
+  if (arrayOfTimeString.length === 2) {
+    console.log('ERROR: timeString has too many \':\'')
+    return dummy
+  }
+
+  let m =  arrayOfTimeString[0],
+      s = arrayOfTimeString[1]
+
+  return {
+    origin: timeString,
+    min: m,
+    sec: s,
+    milli: (60 * m + s) * 60
+  }
+
+
+
+
+}
