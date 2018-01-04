66b09a4bf2aee9d870f9a75f790826a3f549d5eb
YoungHo-Jo <zeroho.jo@gmail.com>
2018-01-04 13:23:46 +0900

    Revert "MyPage 위로 가기 버튼 그림자(Android) 추가, 디자인 변경"
    This reverts commit 424722d0f97713593cd9516497230b68177b607c.

--- Pages/MyPage/index.js
+++ Pages/MyPage/index.js
@@ -37,21 +37,21 @@ const data = [
   {uri: 'https://s-media-cache-ak0.pinimg.com/736x/a5/c9/43/a5c943e02b1c43b5cf7d5a4b1efdcabb.jpg'},
   {uri: 'https://i0.wp.com/www.youbodyhealth.com/wp-content/uploads/2016/08/Delicious-Foods-can-Harm-Your-Brain.jpg?'},
   {uri: 'https://img.buzzfeed.com/buzzfeed-static/static/2017-03/29/15/campaign_images/buzzfeed-prod-fastlane-03/26-delicious-korean-foods-you-need-in-your-life-2-30138-1490814365-13_dblbig.jpg',},
-  {uri: 'https://pbs.twimg.com/media/B59AOmICQAAiGGj.png'},
-  {uri: 'https://img.buzzfeed.com/buzzfeed-static/static/2013-12/enhanced/webdr05/17/17/enhanced-buzz-orig-2548-1387320822-8.jpg'},
+  {uri: 'https://pbs.twimg.com/media/B59AOmICQAAiGGj.png',},
   {text: "For mission text"},
+  {uri: 'https://img.buzzfeed.com/buzzfeed-static/static/2013-12/enhanced/webdr05/17/17/enhanced-buzz-orig-2548-1387320822-8.jpg'},
   {uri: 'https://img.buzzfeed.com/buzzfeed-static/static/2015-03/17/15/enhanced/webdr13/enhanced-6527-1426620797-18.jpg'},
   {uri: 'https://img.buzzfeed.com/buzzfeed-static/static/2014-12/1/15/enhanced/webdr02/enhanced-18393-1417466529-5.jpg'},
   {uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXXTmdaGSOFK8iBeYqoA6_XiQGGWvu6KGnqAxXYyvJA-JKin8ImQ'},
   {uri: 'https://img.buzzfeed.com/buzzfeed-static/static/2015-04/3/15/enhanced/webdr06/enhanced-24427-1428089292-2.jpg'},
-  {text: "In various background color"},
   {uri: 'https://img.buzzfeed.com/buzzfeed-static/static/2016-12/28/12/asset/buzzfeed-prod-web-09/sub-buzz-24236-1482944714-1.jpg'},
+  {text: "In various background color"},
   {uri: 'https://img.buzzfeed.com/buzzfeed-static/static/2016-03/7/17/enhanced/webdr08/enhanced-buzz-8155-1457391039-5.jpg'},
   {uri: 'https://img.buzzfeed.com/buzzfeed-static/static/2017-03/30/12/asset/buzzfeed-prod-fastlane-01/sub-buzz-24597-1490890739-1.jpg'},
   {uri: 'https://img.buzzfeed.com/buzzfeed-static/static/2016-01/14/20/campaign_images/webdr15/which-delicious-mexican-food-item-are-you-based-o-2-20324-1452822970-1_dblbig.jpg'},
   {uri: 'https://img.buzzfeed.com/buzzfeed-static/static/2015-11/30/10/enhanced/webdr15/enhanced-18265-1448896942-17.jpg'},
   {uri: 'https://img.buzzfeed.com/buzzfeed-static/static/2015-12/30/16/enhanced/webdr04/enhanced-15965-1451509932-6.jpg'},
-  {text: "Are you ready to be happy?"},
+  {text: "행복해질 준비가 되었나요?"},
 
 ];
 
@@ -135,7 +135,7 @@ class MyPage extends Component {
         <TouchableHighlight
           style={styles.toTheTopButton}
           underlayColor={'#333333'}>
-          <Text style={styles.topButtonText}>↑</Text>
+          <Text style={styles.topButtonText}>^</Text>
         </TouchableHighlight>
     )
   }
@@ -374,7 +374,7 @@ class MyPage extends Component {
       return (
         <MyPageGridViewTextItem
           text={itemData.text}
-          colorNum={2}
+          colorNum={Math.floor(Math.random()*4)}
           //keyword = {itemData.keyword}
           //missionPK = {itemData.mission_pk}
         />
@@ -529,23 +529,22 @@ const styles = StyleSheet.create({
   },
   toTheTopButton: {
     position: 'absolute',
-    width: 48,
-    height: 48,
-    borderRadius: 24,
-    right: 24,
-    bottom: 45+20,
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    right: 15,
+    bottom: 45+15,
     alignSelf: 'center',
     justifyContent: 'center',
-    backgroundColor: "#00a0eb",
+    backgroundColor: "#ffffff",
     shadowColor: '#000000',
-    shadowRadius: 32,
-    elevation: 3,
+    shadowRadius: 32
   },
   topButtonText: {
-    fontSize: 40,
-    color: '#ffffff',
+    fontSize: 60,
+    color: '#000000aa',
     textAlign: 'center',
-    marginBottom: 8,
+    marginTop: 15,
   }
 
 
