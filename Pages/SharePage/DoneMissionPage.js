bb4b0fb85d5f5b4ed0f2b875c53deb406c4bf76c
JaeHeyK <jaeheyk@gmail.com>
2018-02-06 21:31:41 +0900

    1.DoneMissionPage 의 리스트뷰를 FlatList로 교체

--- Pages/SharePage/DoneMissionPage.js
+++ Pages/SharePage/DoneMissionPage.js
@@ -3,45 +3,122 @@
 import React, { Component, } from 'react';
 
 import {
-    AppRegistry,
-    ListView,
+    FlatList,
     Text,
     StyleSheet,
     View,
-    Image,
+    Dimensions,
 } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import PropTypes from 'prop-types';
 
-import SettingsList from 'react-native-settings-list';
-
-
-class Sharepage_mission extends Component {
+class DoneMissionPage extends Component {
     constructor() {
       super();
     }
 
+    static propTypes = {
+      data: PropTypes.array
+    }
+
+    static defaultProps = {
+      data: [
+        {type: 1, leadText: '삐리삐리.삐리리.'},
+        {type: 2, leadText: '창문 밖에 뭐가 있는 거 같은데,'},
+        {type: 3, leadText: '움켜진 손과는 악수할 수 없는 법'},
+        {type: 1, leadText: '삐리삐리.삐리리.'},
+        {type: 2, leadText: '창문 밖에 뭐가 있는 거 같은데,'},
+        {type: 3, leadText: '움켜진 손과는 악수할 수 없는 법'},
+        {type: 1, leadText: '삐리삐리.삐리리.'},
+        {type: 2, leadText: '창문 밖에 뭐가 있는 거 같은데,'},
+        {type: 3, leadText: '움켜진 손과는 악수할 수 없는 법'},
+        {type: 1, leadText: '삐리삐리.삐리리.'},
+        {type: 2, leadText: '창문 밖에 뭐가 있는 거 같은데,'},
+        {type: 3, leadText: '움켜진 손과는 악수할 수 없는 법'},
+        {type: 1, leadText: '삐리삐리.삐리리.'},
+        {type: 2, leadText: '창문 밖에 뭐가 있는 거 같은데,'},
+        {type: 3, leadText: '움켜진 손과는 악수할 수 없는 법'},
+        {type: 1, leadText: '삐리삐리.삐리리.'},
+        {type: 2, leadText: '창문 밖에 뭐가 있는 거 같은데,'},
+        {type: 3, leadText: '움켜진 손과는 악수할 수 없는 법'},
+        {type: 1, leadText: '삐리삐리.삐리리.'},
+        {type: 2, leadText: '창문 밖에 뭐가 있는 거 같은데,'},
+        {type: 3, leadText: '움켜진 손과는 악수할 수 없는 법'},
+      ]
+    }
+
+    _renderTypeIcon(type) {
+      switch (type) {
+        case 1: return (
+          <Icon
+            name='md-color-palette'
+            size={20}
+            style={styles.typeIcon}/>
+          );
+          break;
+        case 2: return (
+          <Icon
+            name='md-photos'
+            size={20}
+            style={styles.typeIcon}/>
+          );
+          break;
+        case 3: return (
+          <Icon
+            name='md-create'
+            size={20}
+            style={styles.typeIcon}/>
+          );
+          break;
+
+      }
+    }
+
+    _renderRow(rowData) {
+      return (
+        <View style={styles.item}>
+          {this._renderTypeIcon(rowData.type)}
+          <View style={styles.leadTextWrapper}>
+            <Text style={styles.leadText}>{rowData.leadText}</Text>
+          </View>
+          <Icon
+            name='ios-arrow-forward'
+            size={25}
+            style={{color: '#777777'}}
+            />
+        </View>
+      )
+    }
+
+    _renderSeparator() {
+      return (
+        <View style={{height:1, flexDirection: 'row'}}>
+          <View style={{
+              height: 1,
+              width: 12,
+              backgroundColor: '#ffffff'
+            }}/>
+          <View style={{
+              height: 1,
+              width: '100%',
+              backgroundColor: '#bbbbbb'
+            }}/>
+        </View>
+
+      );
+    }
+
+
+
     render() {
         return (
             <View style={{backgroundColor: '#f1f1f1', flex: 1}}>
-                <View style={{flex: 1,marginTop:0}}>
-                    <SettingsList borderColor={'#eaeaea'}>
-                        <SettingsList.Item
-                            title='우주외계인 그려보기'
-                            itemWidth={40}
-                            titleStyle={styles.item}
-                        />
-                        <SettingsList.Item
-                            title='부모님 안보고 그리기'
-                            itemWidth={40}
-                            titleStyle={styles.item}
-                        />
-                        <SettingsList.Item
-                            title='우주쓰레기 그리기'
-                            itemWidth={40}
-                            titleStyle={styles.item}
-                        />
-
-                    </SettingsList>
-                </View>
+              <FlatList
+                style={styles.list}
+                data={this.props.data}
+                renderItem={({item: rowData}) => this._renderRow(rowData)}
+                ItemSeparatorComponent={this._renderSeparator}
+              />
             </View>
         );
     }
@@ -50,19 +127,29 @@ class Sharepage_mission extends Component {
 
 
 const styles = StyleSheet.create({
-    header : {
-        color: '#000000',
-        backgroundColor:'#f1f1f1',
-        height:30,
-        fontSize:15,
-        alignSelf:'center',
-        textAlignVertical:'center'
+    list: {
+      flex: 1,
+      flexDirection: 'column',
     },
-    item:{
-        fontSize:14,
-        paddingLeft:8,
+    item: {
+      flex: 1,
+      flexDirection: 'row',
+      height: 45,
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      paddingHorizontal: 15,
+      backgroundColor: '#ffffff'
     },
-
+    typeIcon: {
+      color: '#777777',
+    },
+    leadTextWrapper: {
+      width: Dimensions.get('window').width - 75
+    },
+    leadText: {
+      fontSize: 15,
+      color: '#333333',
+    }
 });
 
-export default Sharepage_mission;
+export default DoneMissionPage;
